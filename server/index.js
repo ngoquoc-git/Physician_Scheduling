7fda3f362536d3aaccf0cb0c593031b6c775cdb0
Nam Nguyen <namnguyen@cpp.edu>
2021-11-24 00:24:46 -0800

    Fix bugs

--- server/index.js
+++ server/index.js
@@ -93,7 +93,7 @@ app.post("/availability",(req,res) => {
 });
 
 
-app.listen(3001, () => {
-    console.log("Server running on port 3001");
+app.listen(3002, () => {
+    console.log("Server running on port 3002");
 });
 
