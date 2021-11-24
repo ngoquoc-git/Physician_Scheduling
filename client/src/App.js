5a7426f1c3c7f4eaf191045dada0c91e51fb4c6f
Nam Nguyen <namnguyen@cpp.edu>
2021-11-23 23:06:19 -0800

    Add files

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,18 @@
-import React, { useState } from 'react';
 import './App.css';
-import Axios from 'axios';
-
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
+import Login from "./Components/Login";
+import Home from "./Components/Home";
+import SearchBar from "./Components/SearchBar";
 function App() {
-  const [userNameReg, setNameReg] = useState("")
-  const [passwordReg, setPasswordReg] = useState("")
-
-  const register = () => {
-    Axios.post('http://localhost:3001/register', {
-      username: userNameReg,
-      password: passwordReg,
-    }).then((response) => {
-      console.log(response);
-    });
-  };
-
   return (
-    <div className="App">
-      
-     
-      <div>Sign Up</div>
-      <label>Username </label>
-      <input type="text" placeholder="Username"
-        onChange={(e)=>{
-          setNameReg(e.target.value);
-        }} 
-      ></input>
-      <br />
-      <label>Password  </label>
-      <input type="password"
-        onChange={(e)=>{
-          setPasswordReg(e.target.value);
-        }} 
-      ></input>
-      <br />
-      <button onClick={register}>Register</button>    
-
-      
-      
-      <div>Sign In</div>
-      <label>Username </label>
-      <input type="text" placeholder="Username"></input>
-      <br />
-      <label>Password  </label>
-      <input type="password" placeholder="Password"></input>
-      <br />
-      <button type="submit">Log In</button>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/login" exact component={Login}/>
+        <Route path="/search" exact component={SearchBar}/>
+      </Switch>
+    </Router>
+    
   );
 }
 
