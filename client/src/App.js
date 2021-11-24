016085488c7a3acf1b8626ec94fe8a5adda98b51
quocngo-git <ngoquoc.work@gmail.com>
2021-11-23 22:33:52 -0800

    login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+  const [userNameReg, setNameReg] = useState("")
+  const [passwordReg, setPasswordReg] = useState("")
+
+  const register = () => {
+    Axios.post('http://localhost:3001/register', {
+      username: userNameReg,
+      password: passwordReg,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+     
+      <div>Sign Up</div>
+      <label>Username </label>
+      <input type="text" placeholder="Username"
+        onChange={(e)=>{
+          setNameReg(e.target.value);
+        }} 
+      ></input>
+      <br />
+      <label>Password  </label>
+      <input type="password"
+        onChange={(e)=>{
+          setPasswordReg(e.target.value);
+        }} 
+      ></input>
+      <br />
+      <button onClick={register}>Register</button>    
+
+      
+      
+      <div>Sign In</div>
+      <label>Username </label>
+      <input type="text" placeholder="Username"></input>
+      <br />
+      <label>Password  </label>
+      <input type="password" placeholder="Password"></input>
+      <br />
+      <button type="submit">Log In</button>
     </div>
   );
 }
